3b47f31cbfd62f5eea57f11dca532703abbce78b
Daniel123000 <danimonori@gmail.com>
2023-12-12 18:55:09 +0100

    Edited navbar

--- client/src/components/Header/NavBar.jsx
+++ client/src/components/Header/NavBar.jsx
@@ -1,20 +1,13 @@
 function NavBar() {
   return (
-    <div className="navbar_div">
-      <ul className="navbar_ul">
-        <li>
-          <a href="https:\\google.com">GAME</a>
-        </li>
-        <li>
-          <a>PROFILE</a>
-        </li>
-        <li>
-          <a>LOGIN</a>
-        </li>
-        <li>
-          <a>SIGN UP</a>
-        </li>
-      </ul>
+    <div className="navbar">
+      <a class="active" href="javascript:void(0)">
+        Home
+      </a>
+      <a href="javascript:void(0)">Game</a>
+      <a href="javascript:void(0)">Profile</a>
+      <a href="javascript:void(0)">Sign up</a>
+      <a href="javascript:void(0)">Log in</a>
     </div>
   );
 }
