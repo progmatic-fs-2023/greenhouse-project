9d10151fae25f85a1b2eadfe0f58257f18fcb1bd
Daniel123000 <danimonori@gmail.com>
2023-12-13 17:51:25 +0100

    Fixed all package.json for windows

--- client/src/components/Body/Intro.jsx
+++ client/src/components/Body/Intro.jsx
@@ -3,19 +3,19 @@ import '../style.css';
 function Intro() {
   return (
     <div className="body_main_div">
-        <h2>
-          Welcome to the <b>Greenhouse!</b> 🌟
-        </h2>
-        <p>
-          Embark on a journey of knowledge and discovery with our interactive quiz experience. 🚀{' '}
-          <br />
-          Unleash the scholar in you as you dive into a world of questions designed to challenge and
-          enlighten. Whether you're here to sharpen your skills or explore new horizons, you're in
-          for an exciting learning adventure. Ready to begin? Click 'Start Quiz' and let the quest
-          for knowledge commence! Remember: It's not just a quiz; it's a journey of discovery. Happy
-          quizzing! 🚀✨
-        </p>
-      </div>
+      <h2>
+        Welcome to the <b>Greenhouse!</b> 🌟
+      </h2>
+      <p>
+        Embark on a journey of knowledge and discovery with our interactive quiz experience. 🚀{' '}
+        <br />
+        Unleash the scholar in you as you dive into a world of questions designed to challenge and
+        enlighten. Whether you're here to sharpen your skills or explore new horizons, you're in for
+        an exciting learning adventure. Ready to begin? Click 'Start Quiz' and let the quest for
+        knowledge commence! Remember: It's not just a quiz; it's a journey of discovery. Happy
+        quizzing! 🚀✨
+      </p>
+    </div>
   );
 }
 
