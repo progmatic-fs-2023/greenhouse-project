57d449cd653ad9e549e7f4481d797f8cfe05e676
Eszter Sari <60652447+esztusz@users.noreply.github.com>
2023-12-15 02:36:14 +0100

    Updated home page CSS

---
+++ client/src/components/HomepageComponents/WelcomeImg.jsx
@@ -0,0 +1,9 @@
+function WelcomeImg() {
+    return(
+        <div id="welcome_img">
+        {/* <img src="https://picsum.photos/400?grayscale" alt="placeholder_img" />*/}
+      </div>
+    )
+}
+
+export default WelcomeImg;
