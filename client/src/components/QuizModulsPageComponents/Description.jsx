c4980cecd1f2398cec55dc513d2ef268dedc9a3e
Eszter Sari <60652447+esztusz@users.noreply.github.com>
2024-01-03 17:45:57 +0100

    Merging fine-tuning

--- client/src/components/QuizModulsPageComponents/Description.jsx
+++ client/src/components/QuizModulsPageComponents/Description.jsx
@@ -1,6 +1,6 @@
 function Description() {
     return(
-    <div>
+    <div className="description_container">
         <h1>Hello there, coding virtuoso!</h1>
         <p>Ready to elevate your skills in the realms of HTML, CSS, and JavaScript? Choose your topic and 
         unlock challenges tailored to your coding prowess. Specify your preferences and let the coding magic unfold!</p>
