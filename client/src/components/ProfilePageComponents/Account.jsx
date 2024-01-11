97a64d2faff07f6667766d577b8e170f9ec0d826
Eszter Sari <60652447+esztusz@users.noreply.github.com>
2024-01-11 17:45:58 +0100

    fine-tuning

--- client/src/components/ProfilePageComponents/Account.jsx
+++ client/src/components/ProfilePageComponents/Account.jsx
@@ -1,14 +1,10 @@
-import PropTypes from 'prop-types';
 
-export default function Account({ userEmail }){
+export default function Account(){
 
   return (
     <div>
-      <h1>{userEmail}</h1>
+      <h1>userEmail</h1>
     </div>
   );
 }
 
-Account.propTypes = {
-  userEmail: PropTypes.string.isRequired,
-};
