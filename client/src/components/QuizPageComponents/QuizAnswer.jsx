be38df5d44465873b6f3374dbb5d8ec7f577a246
Daniel123000 <danimonori@gmail.com>
2024-01-19 22:21:14 +0100

    More suffer

--- client/src/components/QuizPageComponents/QuizAnswer.jsx
+++ client/src/components/QuizPageComponents/QuizAnswer.jsx
@@ -1,38 +1,3 @@
-/* import './quizpage.css';
-import PropTypes from 'prop-types';
-
-export default function QuizAnswer({ answer, nextQuestion }) {
-  const handleClick = async () => {
-    await nextQuestion(answer.id);
-  };
-
-  return (
-    <div>
-      <button
-        type="button"
-        style={{
-          backgroundColor: answer.hasOwnProperty('isCorrect')
-            ? answer.isCorrect
-              ? 'green'
-              : 'red'
-            : 'white',
-        }}
-        onClick={handleClick}
-      >
-        {answer.name}
-      </button>
-    </div>
-  );
-}
-QuizAnswer.propTypes = {
-  answer: PropTypes.shape({
-    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    name: PropTypes.string.isRequired,
-    isCorrect: PropTypes.bool, // Add this line
-  }).isRequired,
-  nextQuestion: PropTypes.func.isRequired,
-}; */
-
 import './quizpage.css';
 import PropTypes from 'prop-types';
 
