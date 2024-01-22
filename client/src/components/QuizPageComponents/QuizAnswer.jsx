acb0f99a0830b8aaaf6dc9e1c3f395034b744381
Daniel123000 <danimonori@gmail.com>
2024-01-22 19:15:30 +0100

    Hotfixed

--- client/src/components/QuizPageComponents/QuizAnswer.jsx
+++ client/src/components/QuizPageComponents/QuizAnswer.jsx
@@ -1,47 +1,3 @@
-/* import PropTypes from 'prop-types';
-
-export default function QuizAnswer({ answer, nextQuestion }) {
-  const { id, name, isCorrect } = answer;
-
-  const getBackgroundColor = () => {
-    if (isCorrect === true) {
-      return 'green';
-    }
-    if (isCorrect === false) {
-      return 'red';
-    }
-    return 'white';
-  };
-
-  const handleClick = async () => {
-    await nextQuestion(id);
-  };
-
-  return (
-    <div>
-      <button
-        type="button"
-        style={{
-          backgroundColor: getBackgroundColor(),
-        }}
-        onClick={handleClick}
-      >
-        {name}
-      </button>
-    </div>
-  );
-}
-
-QuizAnswer.propTypes = {
-  answer: PropTypes.shape({
-    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    name: PropTypes.string.isRequired,
-    isCorrect: PropTypes.bool,
-  }).isRequired,
-  nextQuestion: PropTypes.func.isRequired,
-};
- */
-
 import PropTypes from 'prop-types';
 
 export default function QuizAnswer({
@@ -92,7 +48,7 @@ QuizAnswer.propTypes = {
     isCorrect: PropTypes.bool,
   }).isRequired,
   nextQuestion: PropTypes.func.isRequired,
-  correctAnswer: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  selectedAnswer: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  correctAnswer: PropTypes.func.isRequired,
+  selectedAnswer: PropTypes.func.isRequired,
   onAnswerClick: PropTypes.func.isRequired,
 };
