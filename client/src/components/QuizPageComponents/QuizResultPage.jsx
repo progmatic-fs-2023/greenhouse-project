517c01d12faf24a3710b741564271995f52e4762
Daniel123000 <danimonori@gmail.com>
2024-01-22 17:40:48 +0100

    FIREWOOORK

--- client/src/components/QuizPageComponents/QuizResultPage.jsx
+++ client/src/components/QuizPageComponents/QuizResultPage.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import './quizpage.css';
+import './animations.css';
 
 function QuizResultPage({ totalQuestions, correctAnswers }) {
   return (
-    <>
-      <div>
-        <h2>Quiz Results</h2>
-        <p>Total Questions: {totalQuestions}</p>
-        <p>Correct Answers: {correctAnswers}</p>
+    <div className="quiz-result-container">
+      <div className="quiz-result-info">
+        <div className="quiz-results-heading">Quiz Results</div>
+        <p className="quiz-results-text">Total Questions: {totalQuestions}</p>
+        <p className="quiz-results-text">Correct Answers: {correctAnswers}</p>
       </div>
-      <div style={{ fontSize: 50 }}>
+      <div className="quiz-result-score" style={{ fontSize: 50 }}>
         <p>
           {correctAnswers} / {totalQuestions}
         </p>
       </div>
-      <div>
+      <div className="quiz-result-button">
         <Link to="/quizmoduls">
-          <button type="button" style={{ height: 50, width: 100 }}>
+          <button type="button" className="new-quiz-button">
             NEW QUIZ
           </button>
         </Link>
       </div>
-    </>
+      <div className="firework-container">
+        <div className="firework" />
+        <div className="firework" />
+        <div className="firework" />
+      </div>
+      <div className="pyro">
+        <div className="before" />
+        <div className="after" />
+      </div>
+    </div>
   );
 }
 
 QuizResultPage.propTypes = {
-  totalQuestions: PropTypes.number.isRequired, // Assume totalQuestions is a number
-  correctAnswers: PropTypes.number.isRequired, // Assume correctAnswers is a number
+  totalQuestions: PropTypes.number.isRequired,
+  correctAnswers: PropTypes.number.isRequired,
 };
 
 export default QuizResultPage;
