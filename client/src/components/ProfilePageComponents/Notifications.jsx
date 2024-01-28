a6ed415305ee117510990b85153dff60c356a3c5
Adam Mihalku <mihalkuadam@gmail.com>
2024-01-28 20:34:47 +0100

    coupon

--- client/src/components/ProfilePageComponents/Notifications.jsx
+++ client/src/components/ProfilePageComponents/Notifications.jsx
@@ -1,12 +1,26 @@
 import './profileMenu.css';
 import { useState, useEffect } from 'react';
+import img from '../../assets/potted_plant.svg';
+import { useAuth } from '../../contexts/AuthContext';
 
 export default function Notifications() {
+  const { startGameUserXp, fetchStartGameUserXp } = useAuth();
   const [isSubscribed, setIsSubscribed] = useState(
     JSON.parse(localStorage.getItem('isSubscribed')) || false,
   );
   const [message, setMessage] = useState('');
+  const [copied, setCopied] = useState(false);
+  const cpnCode = 'GHCDEAL20';
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(cpnCode);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+
+  fetchStartGameUserXp();
   const handleClick = () => {
     const newStatus = !isSubscribed;
     setIsSubscribed(newStatus);
@@ -30,6 +44,28 @@ export default function Notifications() {
         </label>
 
         <p>{message}</p>
+        {startGameUserXp >= 200 ? (
+          <div className="container">
+            <div className="coupon-card">
+              <div className="logo-container">
+                <img src={img} className="logo" alt="logo" />
+                <h3>20% off at Herbalism.com</h3>
+              </div>
+
+              <di className="coupon-row">
+                <span id="cpnCode">{cpnCode}</span>
+                <span id="cpnBtn" onClick={handleCopy}>
+                  {copied ? 'COPIED' : 'COPY CODE'}
+                </span>
+              </di>
+              <p>Valid Till: 20Dec, 2024</p>
+              <div className="circle1" />
+              <div className="circle2" />
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
     </div>
   );
